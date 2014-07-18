77d280faa9837d921beffc4cd764a4fdfc006b0a
Mark Krumholz <mkrumhol@ucsc.edu>
2014-07-18 14:20:08 -0700

    Added __init__.py to slugPy so it can be imported wholesale.

---
+++ slugPy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.1'
+__all__ = ["read_integrated_prop", "read_integrated_spec", 
+           "read_cluster_prop", "read_cluster_spec",
+           "slug_open"]
+
+from read_integrated_prop import read_integrated_prop
+from read_integrated_spec import read_integrated_spec
+from read_cluster_prop import read_cluster_prop
+from read_cluster_prop import read_cluster_prop
+from slug_open import slug_open
