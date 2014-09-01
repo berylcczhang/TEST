021a1cd22820d83c7c88fbcf0deef79f96461391
Michele <michele.fumagalli@durham.ac.uk>
2014-09-01 18:17:52 +0100

    Started devel of test problems

---
+++ test/plot_example.py
@@ -0,0 +1,32 @@
+#
+#This script displays the output of the test problem: example
+#See section 10 of the slug manual 
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import os.path as osp
+import copy
+
+try:
+    from slugpy import *    # If slugpy is already in our path
+except ImportError:
+    # If import failed, try to find slugpy in $SLUG_DIR
+    if 'SLUG_DIR' in os.environ:
+        import sys
+        cur_path = copy.deepcopy(sys.path)
+        sys.path.append(os.environ['SLUG_DIR'])
+        from slugpy import *
+        sys.path = cur_path
+    else:
+        raise ImportError("No module named slugpy")
+
+# Get slug dir
+if 'SLUG_DIR' in os.environ:
+    slugdir = os.environ['SLUG_DIR']
+else:
+    slugdir = '.'
+
+
+# Read runs
