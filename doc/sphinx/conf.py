2919672b054adf6c0e1ae4e0739865eb3902ad65
Mark Krumholz <mkrumhol@ucsc.edu>
2015-02-25 12:50:38 -0800

    Still trying with readthedocs...

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -30,7 +30,7 @@ if on_rtd:
         @classmethod
         def __getattr__(cls, name):
             return Mock()
-    MOCK_MODULES = ['numpy', 'scipy', 'astropy']
+    MOCK_MODULES = ['numpy', 'scipy', 'astropy', 'scipy.interpolate']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
