5db9d4b7d4de3a3221b18614d2e755cb2cc76491
Mark Krumholz <mark.krumholz@gmail.com>
2017-02-27 13:17:23 +1100

    Started to add the ability to have differential nebular / stellar extinction. This checkin just contains code to read from an input file and allocate the extra nebular extinction distribution. It is not yet functional.

--- src/slug_extinction.cpp
+++ src/slug_extinction.cpp
@@ -56,6 +56,23 @@ slug_extinction(const slug_parmParser& pp,
     AVdist = new slug_PDF(pp.get_AV_dist(), rng);
   }
 
+  // See if we are using differential nebular / stellar extinction. If
+  // so, set up the distribution for the excess nebular extinction.
+  if (pp.get_use_neb_excess_extinct()) {
+    // See if the excess extinction is a constant, and, if so, set it
+    // up as a delta function
+    if (pp.get_constant_neb_excess_AV()) {
+      slug_PDF_delta *AV_neb_seg
+	= new slug_PDF_delta(pp.get_neb_excess_AV(), rng);
+      neb_excess_AVdist = new slug_PDF(AV_neb_seg, rng);
+    } else {
+    // Non-constant excess nebular A_V, so read from PDF file
+      neb_excess_AVdist = new slug_PDF(pp.get_neb_excess_AV_dist(), rng);
+    }
+  } else {
+    neb_excess_AVdist = nullptr;
+  }
+
   // Try to open extinction curve file
   ifstream exfile;
   exfile.open(pp.get_extinct_curve());
--- src/slug_parmParser.cpp
+++ src/slug_parmParser.cpp
@@ -126,6 +126,7 @@ slug_parmParser::setDefaults() {
   save_seed = read_seed = false;
   use_extinct = false;
   use_nebular = true;
+  use_neb_excess_extinct = false;
   neb_no_metals = false;
 
   // Physical model parameters
@@ -286,7 +287,7 @@ slug_parmParser::parseFile(std::ifstream &paramFile) {
       } else if (!(tokens[0].compare("a_v"))) {
 	use_extinct = true;
 	try {
-	  // See if the A_V is a number, indicated a constant A_V
+	  // See if the A_V is a number, indicating a constant A_V
 	  A_V = lexical_cast<double>(tokens[1]);
 	  constantAV = true;
 	} catch (const bad_lexical_cast& ia) {
@@ -296,6 +297,17 @@ slug_parmParser::parseFile(std::ifstream &paramFile) {
 	}
       } else if (!(tokens[0].compare("extinction_curve"))) {
 	extinct_curve = tokens[1];
+      } else if (!(tokens[0].compare("a_v_nebular_excess"))) {
+	use_neb_excess_extinct = true;
+	try {
+	  // See if the nebular excess A_V is a number
+	  neb_excess_A_V = lexical_cast<double>(tokens[1]);
+	  constant_neb_excess_AV = true;
+	} catch (const bad_lexical_cast& ia) {
+	  // Not a number, so assume it is a distribution file name
+	  neb_excess_AV_dist = tokens[1];
+	  constant_neb_excess_AV = false;
+	}	
       } else if (!(tokens[0].compare("atomic_data"))) {
 	atomic_dir = tokens[1];
       } else if (!(tokens[0].compare("compute_nebular"))) {
@@ -792,8 +804,13 @@ bool slug_parmParser::get_logTime() const { return logTime; }
 bool slug_parmParser::get_constantSFR() const { return constantSFR; }
 bool slug_parmParser::get_randomSFR() const { return randomSFR; }
 bool slug_parmParser::get_constantAV() const { return constantAV; }
+bool slug_parmParser::get_use_neb_excess_extinct() const
+{ return use_neb_excess_extinct; }
+bool slug_parmParser::get_constant_neb_excess_AV() const
+{ return constant_neb_excess_AV; }
 double slug_parmParser::get_SFR() const { return sfr; }
 double slug_parmParser::get_AV() const { return A_V; }
+double slug_parmParser::get_neb_excess_AV() const { return neb_excess_A_V; }
 double slug_parmParser::get_z() const { return z; }
 double slug_parmParser::get_WR_mass() const { return WR_mass; }
 double slug_parmParser::get_metallicity() const { return metallicity; }
@@ -813,6 +830,8 @@ const char *slug_parmParser::get_extinct_curve() const
 { return extinct_curve.c_str(); }
 const char *slug_parmParser::get_AV_dist() const 
 { return AV_dist.c_str(); }
+const char *slug_parmParser::get_neb_excess_AV_dist() const 
+{ return neb_excess_AV_dist.c_str(); }
 const char *slug_parmParser::get_outtime_dist() const 
 { return out_time_dist.c_str(); }
 const char *slug_parmParser::get_filter_dir() const 
